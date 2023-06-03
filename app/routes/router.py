301b2c8f0e93d446ba2cecd90d553c1d97b736eb
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-06-03 16:38:27 -0600

    corta errores añadidos

--- app/routes/router.py
+++ app/routes/router.py
@@ -64,7 +64,7 @@ async def reciveImage(file: UploadFile = File(...)):
 
 
 # este endpont es de desarrollo, aun no pidas cosas acá
-@router.post("/API/Hide/", tags=["Recive Imagen"])
+@router.post("/API/Hide/dev", tags=["Recive Imagen"])
 async def reciveImage(file: UploadFile = File(...)):
     if file.filename[-4:] in imgFormats:
         # Uno la ruta de imgFolder con el nombre del archivo menos la extensión
@@ -109,30 +109,93 @@ async def reciveImage(file: UploadFile = File(...)):
         )
 
 
-@router.post("/API/Unhide/", tags=["Recive Imagen"])
-async def decrypt_image(file: UploadFile = File(...)):
-    if file.filename[-4:] in cifFormats:
-        # Uno la ruta de imgCifFolder con el nombre del archivo menos la extensión
-        file_folder = os.path.join(imgCifFolder, file.filename[:-4])
-        # Creo la ruta final del archivo
-        os.makedirs(file_folder, exist_ok=True)
-        # Guardo el archivo dentro de la carpeta
-        file_path = os.path.join(file_folder, file.filename)
-        with open(file_path, "wb") as F:
-            content = await file.read()
-            F.write(content)
-            F.close()
-        res_merge = await merge_img.merge(file_path, file.filename)
-        print(file_path)
-        if res_merge["success"] == True:
-            return {"Success": res_merge["success"]}
+@router.post("/API/Hide/", tags=["Recive Imagen"])
+async def reciveImage(file: UploadFile = File(...)):
+    try:
+        if file.filename[-4:] in imgFormats:
+            # Uno la ruta de imgFolder con el nombre del archivo menos la extensión
+            file_folder = os.path.join(imgFolder, file.filename[:-4])
+            # Creo la ruta final del archivo
+            os.makedirs(file_folder, exist_ok=True)
+            # Guardo el archivo dentro de la carpeta
+            file_path = os.path.join(file_folder, file.filename)
+            with open(file_path, "wb") as F:
+                content = await file.read()
+                F.write(content)
+                F.close()
+            res_divide = await divide_img.divide(file_path, file.filename)
+            # Respondo un archivo con la dirección de guardado
+            if res_divide["success"] == True:  # esto también debería ir en un try catch
+                res_hide = await hide_img(
+                    res_divide["img_yfile"],
+                    res_divide["img_cbmin"],
+                    res_divide["img_crmin"],
+                )
+                if res_hide["success"] == True:
+                    return FileResponse(res_hide["y-hided"])
+                    # Acá comienzo el cifrado
+                    # res_cif = await cypher_image(clave, iv, file_path, file.filename)
+                else:
+                    return JSONResponse(
+                        content={
+                            "Error": res_hide["error"],
+                        }
+                    )
+                res_uncif = await decipher_image(clave, iv, file_path, file.filename)
+            else:
+                return JSONResponse(
+                    content={
+                        "Error": res_divide["error"],
+                    },
+                    status_code=415,
+                )
         else:
             return JSONResponse(
-                content={
-                    "Error": res_merge["error"],
-                }
+                content={"Error": "La extención del archivo no es válida"},
+                status_code=415,
             )
-    else:
+    except:
         return JSONResponse(
-            content={"Error": "la extención del archivo no es válida"}, status_code=415
+            content={
+                "Error": "Hay un error desconocido con el archivo, intentelo de nuevo más tarde"
+            },
+            status_code=415,
+        )
+
+
+@router.post("/API/Unhide/", tags=["Recive Imagen"])
+async def decrypt_image(file: UploadFile = File(...)):
+    try:
+        if file.filename[-4:] in cifFormats:
+            # Uno la ruta de imgCifFolder con el nombre del archivo menos la extensión
+            file_folder = os.path.join(imgCifFolder, file.filename[:-4])
+            # Creo la ruta final del archivo
+            os.makedirs(file_folder, exist_ok=True)
+            # Guardo el archivo dentro de la carpeta
+            file_path = os.path.join(file_folder, file.filename)
+            with open(file_path, "wb") as F:
+                content = await file.read()
+                F.write(content)
+                F.close()
+            res_merge = await merge_img.merge(file_path, file.filename)
+            print(file_path)
+            if res_merge["success"] == True:
+                return {"Success": res_merge["success"]}
+            else:
+                return JSONResponse(
+                    content={
+                        "Error": res_merge["error"],
+                    }
+                )
+        else:
+            return JSONResponse(
+                content={"Error": "la extención del archivo no es válida"},
+                status_code=415,
+            )
+    except:
+        return JSONResponse(
+            content={
+                "Error": "Hay un error desconocido con el archivo, intentelo de nuevo más tarde"
+            },
+            status_code=415,
         )
